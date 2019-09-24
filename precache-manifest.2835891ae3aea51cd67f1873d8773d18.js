8efe56353c8b52f58687cfd42ca2e418e4f60983
Viola Hu <lovelyviolaat@hotmail.com>
2019-09-24 10:06:03 +1000

    Updates

---
+++ precache-manifest.2835891ae3aea51cd67f1873d8773d18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaf536be2ea3e4b5614346a7400a89d",
+    "url": "/OxfordHackathon2019/index.html"
+  },
+  {
+    "revision": "9f6c841c35a19341108b",
+    "url": "/OxfordHackathon2019/static/css/main.12dab27e.chunk.css"
+  },
+  {
+    "revision": "ec3f30aea2f4f584db44",
+    "url": "/OxfordHackathon2019/static/js/2.c918cf06.chunk.js"
+  },
+  {
+    "revision": "9f6c841c35a19341108b",
+    "url": "/OxfordHackathon2019/static/js/main.bd8182d4.chunk.js"
+  },
+  {
+    "revision": "1252a42856b032683c1d",
+    "url": "/OxfordHackathon2019/static/js/runtime~main.0cf50eff.js"
+  },
+  {
+    "revision": "0cbd653de0f4f0e9f7c779c947bfef4e",
+    "url": "/OxfordHackathon2019/static/media/leftLightening.0cbd653d.png"
+  }
+]);
