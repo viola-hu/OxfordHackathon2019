cc851f950fbd8e303271d501273bc5c419d2a687
Viola Hu <lovelyviolaat@hotmail.com>
2019-10-27 00:50:58 +1100

    changed navbar

--- src/components/BootNav.jsx
+++ src/components/BootNav.jsx
@@ -11,11 +11,11 @@ class BootNav extends Component{
   render(){
     return(
       <Navbar className='nav-image' xpand="lg">
-        <Navbar.Brand style={{color: 'white'}} href="#home"><Image style={{maxHeight:'2.5em'}} src={process.env.PUBLIC_URL +`/oxford.png`}/> </Navbar.Brand>
+        <Navbar.Brand style={{color: 'white'}} href="/"><Image style={{maxHeight:'2.5em'}} src={process.env.PUBLIC_URL +`/oxford.png`}/> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link style={{color: 'white'}} href="#home">Home</Nav.Link>
+            <Nav.Link style={{color: 'white'}} href="/">Home</Nav.Link>
             <Nav.Link style={{color: 'white'}} href="#link">Link</Nav.Link>
           </Nav>
         </Navbar.Collapse>
